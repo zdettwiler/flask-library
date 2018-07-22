82ff9c2df38641adb4d6d920e8907f99de1cfb00
Zach Dettwiler <zach@ZachMacBookAir.local>
2018-07-22 14:16:31 +0100

    created Reset Password forms

--- flasklibrary/users/forms.py
+++ flasklibrary/users/forms.py
@@ -43,3 +43,17 @@ class UpdateAccountForm(FlaskForm):
 			user = User.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError("You've already got an account with this email. Please log in!")
+
+class RequestResetForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	submit = SubmitField('Request Password Reset')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user == None:
+			raise ValidationError("No account exists with this email. Please create an account!")
+
+class ResetPasswordForm(FlaskForm):
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Reset Password')
